cf2980074c400d8943797e01f1a1d35527fec21d
RuchiraCodeX <ruchiramadhu2001@gmail.com>
2024-09-08 19:13:23 +0530

    Update User Model
    Add validations

--- src/main/java/com/example/abc_restaurant/Models/User.java
+++ src/main/java/com/example/abc_restaurant/Models/User.java
@@ -24,18 +24,18 @@ public class User {
     private String id;
     private String userID;
 
-    @NotBlank(message = "Username is required")
+    @NotBlank(message ="Username is required")
     @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
     private String username;
 
-    @NotBlank(message = "Password is required")
+    @NotBlank(message ="Password is required")
     @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
 
-    @NotBlank(message = "Role is required")
+    @NotBlank(message ="Role is required")
     private String role;
 
-    @Email(message = "Invalid email format")
+    @Email(message ="Invalid email format")
     @NotBlank(message = "Email is required")
     private String email;
     private boolean active;
